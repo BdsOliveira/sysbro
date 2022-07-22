6e7a6e4b8d8a5d0a67637bb3c1f7b7226867fb87
BdsOliveira <bds.commus@gmail.com>
2022-07-21 21:03:42 -0300

    Restructure form to insert new sells

--- public/js/createSell.js
+++ public/js/createSell.js
@@ -23,15 +23,12 @@ async function resquest() {
         })
 }
 async function createSell(newSell = {}) {
-    console.log(newSell)
     let sellObject = {
         "nameSeller": `${newSell.nameSeller}`,
         "companySeller": `${newSell.companySeller}`,
-        "quantityClients": `${newSell.quantityClients}`,
-        "quantityEmploye": `${newSell.quantityEmploye}`,
+        "quantitySold": `${newSell.quantitySold}`,
         "date": `${new Date(newSell.date).toISOString()}`,
     }
-    console.log(sellObject)
     let response = await fetch("https://sysbro.herokuapp.com/sells", {
         method: 'POST',
         headers: {
@@ -49,20 +46,16 @@ function getSellObjectFromForm() {
     let form = document.getElementById("formCreateSell");
     newSell.nameSeller = form.elements[0].value;
     newSell.companySeller = form.elements[1].value;
-    newSell.quantityClients = form.elements[2].value;
-    newSell.quantityEmploye = form.elements[3].value;
-    newSell.date = form.elements[4].value;
+    newSell.quantitySold = form.elements[2].value;
+    newSell.date = form.elements[3].value;
     if (newSell.nameSeller === '' || newSell.nameSeller === null) {
         printMessage('Nome do Vendedor');
         return -1;
     } else if (newSell.companySeller === '' || newSell.companySeller === null) {
         printMessage('Empresa do vendedor');
         return -1;
-    } else if (newSell.quantityClients === '' || newSell.quantityClients === null) {
-        printMessage('Quantidade vendida para clientes');
-        return -1;
-    } else if (newSell.quantityEmploye === '' || newSell.quantityEmploye === null) {
-        printMessage('Quantidade vendida para lojistas');
+    } else if (newSell.quantitySold === '' || newSell.quantitySold === null) {
+        printMessage('Quantidade vendida');
         return -1;
     } else if (newSell.date === '' || newSell.date === null) {
         printMessage('Data da Venda');
